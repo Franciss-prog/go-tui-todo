ae2d4e86bd5a17d9e91f40166b6e5bf96a61e8f2
Franciss-prog <francisgil.prog@gmail.com>
2026-05-03 21:33:06 +0800

    adding some model and init the model

--- model.go
+++ model.go
@@ -1,1 +1,17 @@
 package main
+
+type todo struct {
+	task     []string
+	cursor   int
+	done     bool
+	selected map[int]struct{}
+}
+
+func initTodo() todo {
+	return todo{
+		task:     []string{},
+		cursor:   0,
+		done:     false,
+		selected: make(map[int]struct{}),
+	}
+}
